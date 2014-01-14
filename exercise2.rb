1bb571a8b35def909716bc13f98765c8bc839a94
bshain <shainbarbara@gmail.com>
2014-01-14 14:05:06 -0500

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+tip = 55*0.2
+tip=tip.to_i.to_s
+puts "I think a good tip on a $55 meal would be $#{tip}."
+
+puts "6" + 6.to_s
+
+product= 45628*7839
+puts "The product is #{product}."
+
+puts "Predition: false"
+puts (true && false) || (false && true) || (false && false)
