a726c50dda0baf16a3854562cd1da2f5c6b70a2a
bshain <shainbarbara@gmail.com>
2014-01-14 15:30:11 -0500

    Exercise 3.1

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What's your name?" 
-name=gets.chomp
+name = gets.chomp
 puts "Hi #{name}!"
+puts "How old are you, #{name}?"
+age = gets.chomp
+year_born= (Time.now.year) - age.to_i
+puts "You were born in #{year_born}"
